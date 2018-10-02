79fc9b968d61afe9d10cdd32b68e3412d0d0a89a
Johan Gudmundsson <johan.gudmundsson@vbu.ludvika.se>
2018-10-02 08:40:14 +0200

    init

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,11 @@
 /* eslint-disable no-unused-vars */
 import style from '../css/style.css';
 /* eslint-enable no-unused-vars */
+const btnEl = document.getElementById('btn');
+const textEl = document.getElementById('text');
+const inputEl = document.getElementById('input');
+
+btnEl.addEventListener('click', () => {
+  const text = inputEl.value;
+  textEl.innerHTML = text;
+});
